3facb5939f383c299ab24db50558eaced34f772e
jmjones03 <jacobmjones03@gmail.com>
2025-08-04 11:26:43 -0400

    added img to page react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/Jacob.png"
+          alt="name"
           width={180}
           height={38}
           priority
@@ -15,7 +15,6 @@ export default function Home() {
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Jacob Jones
-            .
           </li>
           <li className="tracking-[-.01em]">
             Portfolio
