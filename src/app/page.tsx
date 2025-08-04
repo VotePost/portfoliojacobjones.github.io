603b75e39b13500ea7edde011fed0b5289a3a698
jmjones03 <jacobmjones03@gmail.com>
2025-08-04 13:16:48 -0400

    reduced gap between image and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-2 row-start-2 items-center sm:items-start">
+        <div>
         <Image
           src="/Jacob.png"
           alt="name"
@@ -18,7 +19,7 @@ export default function Home() {
             Portfolio
           </li>
         </ol>
-
+        </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
